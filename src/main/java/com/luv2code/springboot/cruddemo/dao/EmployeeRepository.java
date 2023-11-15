b93b222225f96eb97055cef4f2633c3b2472c4fa
luke-brandt <73630972+luke-brandt@users.noreply.github.com>
2023-11-15 08:23:07 -0700

    Update EmployeeRepository.java

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it ... no need to write any code LOL!
+    
 
 }
